88904d27668a41603d0c408d4f23e26ed3c673df
hdbg <90869971+hdbg@users.noreply.github.com>
2022-12-05 21:24:26 +0200

    Update jit.nim and vm.nim

--- src/mut/vm.nim
+++ src/mut/vm.nim
@@ -4,104 +4,38 @@ import shared
 
 # Intermidiate Representation
 type
-  IRKind* {.pure.} = enum
-    LoadVar
-    StoreVar
+  IROp {.pure.} = enum
+    MovFrom               # Copy from stack by offset
+    MovTo                 # Copy to stack by offset
+    
 
-    Add
-    Sub
-    Mul
-    Div
+#[
+  Steps:
+    - Walk through all the stack allocation and assign offset to each symbol depending on its size
+    - Convert all expressions to IR
+    - Convert control flow to basic jumps
+]#
 
-    Or
-    And
-    Xor
-    Not
-
-    CallExternal
-
-    Push
-    Pop
-
-    Jump
-
-  IROperand* {.pure.} = enum
-    Immediate
-    Var
-    None
-
-  IRJumpKind* {.pure.} = enum
-    Less
-    Greater
-    Equals
-    NotEquals
-
-  IRFuncArgKind {.pure.} = enum
-    ReturnValPtr
-    ExternalProcPtr
-    Real
-
-  IRReg {.pure.} = enum
-    A
-    B
-    C
-
-
-  IRInstruction = object
-    case kind: IRKind
-    of Jump:
-      jumpKind: IRJumpKind
-    of LoadVar:
-      target: IRReg
-    else: discard
-    op: IROperand
-
-  
-  IRBasicBlock = object
-    virtualAddr: uint
-    instr: seq[IRInstruction]
-
-
-# Actual VM
 type
-  VMWord = int64
+  VarEntry = object
+    offset: int
+    name, kind: NimNode
 
-  VMFunction = object
-    rawBody: NimNode
-    args: seq[IRFuncArgKind]
-
-    bbs: seq[IRBasicBlock]
-    identToSlot: Table[NimNode, uint]
-
-proc compileInfix(inf: NimNode, retValIn: IrReg = IrReg.A): seq[IRInstruction] {.compileTime.} =
-  let
-    op = inf[0]
-    lhs = inf[1]
-    rhs = inf[2]
-
-
-macro virtualize*(fun: typed): NimNode = 
-  result.expectKind nnkProcDef
-
-  let 
-    params = fun[3] # nnkProcDef -> nnkFormalParams
-    retType = params[0]
-
-    funcBody = fun[6]
+  CompilationCtx = ref object
+    vars: seq[VarEntry]
 
 when isMainModule:
-  dumpAstGen():
-    type
-      TestObj = object
-        x: int
+  dumpAstGen:
+    proc x = 
+      let 
+        d = 4
+        b = VarEntry()
+      var 
+        t = 4
+        y = 10
 
-    proc toVirt(x, y, z: int, test: TestObj): int = 
-      result = x + y - y * 2 div 4 * 8
+      t += 10
 
-      if x > y:
-        result = z
-
-      test.x = y
-
+      t = t + y * 10 / 2
 
 
